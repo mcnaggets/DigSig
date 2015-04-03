440e19b07b3ae7b69546f6a6713b70397ec3927b
Maksim Prabarshchuk <Maksim_Prabarshchuk@sap.com>
2015-04-03 18:09:29 +0300

    gradle build support

--- src/main/java/by/kate/ui/JavaFXApplication.java
+++ src/main/java/by/kate/ui/JavaFXApplication.java
@@ -21,7 +21,7 @@ public class JavaFXApplication extends Application {
 
         Scene scene = new Scene(root, 800, 600);
 
-        stage.setTitle("Welcome");
+        stage.setTitle("Цифровая подпись");
         stage.setScene(scene);
         stage.show();
     }
